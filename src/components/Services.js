ee58732c547c76ccf09a7a2ea430d710988f3a97
tyler <tymaggio03@gmail.com>
2022-02-21 12:28:26 -0700

    portfolio finished styling needs work

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,5 @@
 import React from "react";
+//Font Awesome Imports
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookF, faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { faDesktop, faFileCode } from "@fortawesome/free-solid-svg-icons";
